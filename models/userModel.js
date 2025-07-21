6427bbfc687aa8b10de6210b7e3cbc6337cdea96
Markiza666 <3ggjs24erikma@folkuniversitetet.nu>
2025-07-21 19:35:56 +0200

    Updated comments.

--- models/userModel.js
+++ models/userModel.js
@@ -67,9 +67,9 @@ userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
-    });
+});
 
-    // Instance method to compare an entered password with the hashed password in the database
+// Instance method to compare an entered password with the hashed password in the database
     userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
